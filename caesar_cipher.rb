bd99fca60260e9c54d04bfb6bcfa861f8fe0af82
sweetzachary <a.ntigr16@gmail.com>
2021-12-03 13:20:17 +0200

    Remove debugging functionality

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,3 @@
-require 'pry-byebug'
-
 def caesar_cipher(str, offset)
     str.split('').map {|c| shift_letter c, offset}.join('')
 end
@@ -8,7 +6,6 @@ def shift_letter(l, offset)
     types = {upper: Array('A'..'Z'),
              lower: Array('a'..'z'),
              number: Array('0'..'9')}
-    binding.pry
     type = types.filter_map {|t, val| t if val.include? l}[0]
     if type != nil && l.ord + offset > types[type].last.ord then
         (types[type].first.ord - 1 + (l.ord + offset - types[type].last.ord)).chr
